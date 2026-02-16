f2de987530c4ddc6e1f2220b50630ea2185d16d5
sundxy11043-00 <sunday11043@gmail.com>
2026-02-16 15:06:53 +0700

    fix session cookie

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: { 
-    secure: false,
-    sameSite: 'none',
+    secure: true,
+    sameSite: 'lax',
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000 
   }
